d4495611ee0494f23aa718563b362194146004ca
arturopm23 <arturopermeg23@gmail.com>
2024-09-25 09:05:58 +0200

    little hotfix on the seeder where before the win didnt depend on the dice result

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,9 +38,9 @@ class DatabaseSeeder extends Seeder
         foreach ($users as $user) {
             Game::factory()->count(3)->create([
                 'user_id' => $user->id,
-                'dice1' => rand(1, 6),
-                'dice2' => rand(1, 6),
-                'win' => (rand(0, 1) == 1),
+                'dice1' => $dice1 = rand(1, 6),
+                'dice2' => $dice2 = rand(1, 6),
+                'win' => ($dice1 + $dice2 == 7),
             ]);
         }
     }
